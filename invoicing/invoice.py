6a75f71a7817da897fe0872190878f837dfe8d16
Rohit_More <morer4851@gmail.com>
2026-01-01 15:03:35 +0530

    Final Commit: Uploaded the package to PyPI

--- invoicing/invoice.py
+++ invoicing/invoice.py
@@ -75,7 +75,7 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
 		# Add company name and logo
 		pdf.set_font(family="Times", size=14, style="B")
 		pdf.cell(w=25, h=8, txt=f"PythonHow")
-		pdf.image("pythonhow.png", w=10)
+		pdf.image(image_path, w=10)
 		
 		if not os.path.exists(pdfs_path):
 			os.makedirs(pdfs_path)
